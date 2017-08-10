cab76001c23047a32ccfa192b13be72ac01a8f0c
jgw96 <jgw9617@gmail.com>
2017-08-10 14:12:02 -0500

    chore(): add prefetch

--- www/build/app.js
+++ www/build/app.js
@@ -1,20 +1,3 @@
 /*! Built with http://stenciljs.com */
 
-(function (window, document, appNamespace, appCore, appCoreEs5, components, x) {
-    'use strict';
-    // create global namespace if it doesn't already exist
-
-    (window[appNamespace] = window[appNamespace] || {}).components = components = components || [];
-    // auto hide components until they been fully hydrated
-    // reusing the "x" variable from the args for funzies
-    x = document.createElement('style');
-    x.innerHTML = (components.map(function (c) {
-        return c[0];
-    }).join(',') + '{visibility:hidden}.💎{visibility:inherit}').toLowerCase();
-    x.innerHTML += 'ion-app:not(.💎){display:none}';
-    document.head.appendChild(x);
-    // request the core file this browser needs
-    x = document.createElement('script');
-    x.src = window.customElements ? appCore : appCoreEs5;
-    document.head.appendChild(x);
-})(window, document, "App","build/app/app.core.js","build/app/app.core.ce.js",[["FIBER-DEMO","fiber-demo",0,[["elapsed",1,2]]],["FIBER-DOT","fiber-demo",0,[["size",1,2],["text",1],["x",1,2],["y",1,2]]],["FIBER-TRIANGLE","fiber-demo",0,[["s",1,2],["seconds",1,2],["x",1,2],["y",1,2]]]]);
+!function(e,n,i,t,p,o,s){(e.App=e.App||{}).components=o=o||[],(s=n.createElement("style")).innerHTML=(o.map(function(e){return e[0]}).join(",")+"{visibility:hidden}.💎{visibility:inherit}").toLowerCase(),s.innerHTML+="ion-app:not(.💎){display:none}",n.head.appendChild(s),(s=n.createElement("script")).src=e.customElements?"build/app/app.0fzjqob7.js":"build/app/app.b66btnel.ce.js",n.head.appendChild(s)}(window,document,0,0,0,[["FIBER-DEMO","ncohzt3g",0,[["elapsed",1,2]]],["FIBER-DOT","ncohzt3g",0,[["size",1,2],["text",1],["x",1,2],["y",1,2]]],["FIBER-TRIANGLE","ncohzt3g",0,[["s",1,2],["seconds",1,2],["x",1,2],["y",1,2]]]]);
